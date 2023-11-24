e9ca6729d1266844bb080d018666cf0689b5b620
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-11-24 15:37:28 +0300

    Add the view for splash page index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,19 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    @group_transactions = GroupTransaction.all
-    @user = current_user
+    if current_user
+      # Retrieve the user along with their associated categories and purchases.
+      @user = User.includes(group_transactions: :money_transactions).find(current_user.id)
+      @totals = []
+      @user.group_transactions.each do |categ|
+        total_amt = 0
+        categ.money_transactions.each do |purch|
+          total_amt += purch.amount
+        end
+        @totals << total_amt
+      end
+    else
+      redirect_to new_user_session_path, notice: 'Please sign in to view your categories.'
+    end
   end
 end
--- app/models/group_transaction.rb
+++ app/models/group_transaction.rb
@@ -1,7 +1,7 @@
 class GroupTransaction < ApplicationRecord
-  belongs_to :user
   validates :name, presence: true
   validates :icon, presence: true
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   has_many :category_purchase_transactions
   has_many :money_transactions, through: :category_purchase_transactions
