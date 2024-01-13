5622b1ca529571fa0c258eab570ff7d9081d9691
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-01-13 22:09:25 +0300

    Fix the links to splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  get 'splash/index'
   get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
   devise_for :users
+
+  authenticated :user do
+    root "home#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    devise_scope :user do
+      root "splash#index", as: :unauthenticated_root
+    end
+  end
+
   resources :group_transactions do
     resources :money_transactions, only: [:create, :new]
   end
-  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
